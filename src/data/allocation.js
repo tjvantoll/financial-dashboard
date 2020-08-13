66ceb4a099eae5e33fd4f00124558f32a24dc24b
TJ VanToll <tj.vantoll@gmail.com>
2020-08-13 15:51:17 -0400

    Switching to use a remote API

--- src/data/allocation.js
+++
@@ -1,6 +0,0 @@
-export const allocation = [
-  { category: "Domestic", value: 48.5 },
-  { category: "International", value: 14.4 },
-  { category: "Bonds", value: 17.0 },
-  { category: "Cash", value: 20.1 },
-];
