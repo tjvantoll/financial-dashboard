0589952d0796cc6c54910de321a597bcbdb251c9
TJ VanToll <tj.vantoll@gmail.com>
2020-10-30 16:45:40 -0400

    Switching back to using hardcoded data

---
+++ src/data/info.js
@@ -0,0 +1,119 @@
+export const info = {
+  "managers": [
+    {
+      firstName: "Nora",
+      lastName: "Georgieva",
+      position: "Lead Investor"
+    }, {
+      firstName: "Carl",
+      lastName: "Bergenhem",
+      position: "Advisor"
+    }, {
+      firstName: "Kiril",
+      lastName: "Peyanski",
+      position: "Senior Analyst"
+    }, {
+      firstName: "Pamela",
+      lastName: "Petkova",
+      position: "Senior Analyst"
+    }, {
+      firstName: "Vasil",
+      lastName: "Raev",
+      position: "Junior Associate"
+    }, {
+      firstName: "Genady",
+      lastName: "Sergeev",
+      position: "Trainee"
+    }
+  ],
+  "quarters": [
+    {
+      title: "Q1 2020",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q4 2019",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q3 2019",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q2 2019",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q1 2019",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q4 2018",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q3 2018",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    },
+    {
+      title: "Q2 2018",
+      details: [
+        { name: "Expense Ratio", value: "0.13%" },
+        { name: "Minimum Investment", value: "$0.00" },
+        { name: "Turnover Rate", value: "11%" },
+        { name: "Portfolio Assets", value: "$5,533M" },
+        { name: "12-Month Low", value: "$35.45" },
+        { name: "12-Month High", value: "$50.59" }
+      ]
+    }
+  ]
+}
--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,19 +1,43 @@
+import { info } from "../data/info";
+import { allocation } from "../data/allocation";
+import { performance } from "../data/performance";
+import { positions } from "../data/positions";
+import { Allocation, Position } from "../data/models";
+
+function getRandomDelay() {
+  // return 0;
+  return 500 + (Math.random() * 2000);
+  // return 9999999999;
+}
+
 export function getFundInfo(): Promise<any> {
-  return fetch("https://demoapis.com/sample/finance/info")
-    .then(response => response.json());
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(info);
+    }, getRandomDelay());
+  })
 }
 
-export function getFundAllocation(): Promise<any> {
-  return fetch("https://demoapis.com/sample/finance/allocation")
-    .then(response => response.json());
+export function getFundAllocation(): Promise<Allocation[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(allocation);
+    }, getRandomDelay());
+  });
 }
 
-export function getPerformance(): Promise<any> {
-  return fetch("https://demoapis.com/sample/finance/performance")
-    .then(response => response.json());
+export function getPerformance(): Promise<string[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(performance);
+    }, getRandomDelay());
+  })
 }
 
-export function getPositions(): Promise<any> {
-  return fetch("https://demoapis.com/sample/finance/positions")
-    .then(response => response.json());
+export function getPositions(): Promise<Position[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(positions);
+    }, getRandomDelay());
+  });
 }
